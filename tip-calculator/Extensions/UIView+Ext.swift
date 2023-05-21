80ca74dfbf798e261f9264c6393d3272b9181629
Chukwubuikem Chukwudi <myecodevelopment@gmail.com>
2023-05-21 22:39:44 +0100

    Creating tip Views

---
+++ tip-calculator/Extensions/UIView+Ext.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+Ext.swift
+//  tip-calculator
+//
+//  Created by Eco Dev System on 21/05/2023.
+//
+
+import UIKit
+
+extension UIView {
+    func addShadow(offset: CGSize, color: UIColor, radius: CGFloat, opacity: Float) {
+        layer.cornerRadius = radius
+        layer.masksToBounds = false
+        layer.shadowOffset = offset
+        layer.shadowColor = color.cgColor
+        layer.shadowRadius = radius
+        layer.shadowOpacity = opacity
+        let backgroundCGColor = backgroundColor?.cgColor
+        backgroundColor = nil
+        layer.backgroundColor = backgroundCGColor
+    }
+}
